277c00eb3025816552b3b48d78a5caf87dcb3f04
junhakang <junhakang5730@gmail.com>
2021-01-09 19:36:46 +0900

    commit

--- src/app.js
+++ src/app.js
@@ -1,15 +1,22 @@
-function myFun() {
-  console.log("Func!!");
-  console.log("Func!!");
-  console.log("Func!!");
-  console.log("Func!!");
-  console.log("Func!!");
-  console.log("Func!!");
-  console.log("Func!!");
+let twoSum = function(nums, target) {
+    
+  let firstNum = null;
+  let secondNum = null;
+  
+  for (num of nums) {
+      
+      for ( let i = nums.indexOf(num)+1; i < nums.length; i++ ){
+          if (num + nums[i] == target) {
+              console.log(i)
+              firstNum = nums.indexOf(num);
+              secondNum = i;
 
-  console.log("Func!!");
-  console.log("Func!!");
+              return [firstNum , secondNum];
+              } 
+          }        
+      }
+};
 
-  console.log("Func!!");
-  console.log("Func!!");
-}
+
+console.log(twoSum([3,2,4], 6))
+// Expected : [1,2]
